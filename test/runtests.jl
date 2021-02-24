8389446e1609267954c96567ed44074dc4fa4574
Changhyun Kwon <chkwon@users.noreply.github.com>
2021-02-24 09:33:23 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,9 +11,9 @@ using Test
 
 @testset "Concorde.jl" begin
     M = [
-        0  16   7  14
+         0  16   7  14
         16   0   3   5
-        7   3   0  16
+         7   3   0  16
         14   5  16   0 
     ]
     opt_tour, opt_len = solve_tsp(M)
